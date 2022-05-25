43842c6b616d7d65aa226c5201b381ad3ac160dc
l3aro <dgbao1340@gmail.com>
2022-05-25 20:33:32 +0700

    allow custom inline scope

--- src/Concerns/Filterable.php
+++ src/Concerns/Filterable.php
@@ -2,14 +2,16 @@
 
 namespace Baro\PipelineQueryCollection\Concerns;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Pipeline\Pipeline;
 
 trait Filterable
 {
-    public function scopeFilter($query)
+    public function scopeFilter(Builder $query, array $criteria = null): Builder
     {
+        $criteria = is_null($criteria) ? $this->getFilters() : $criteria;
         return app(Pipeline::class)
             ->send($query)
-            ->through($this->getFilters());
+            ->through($criteria);
     }
 }
--- src/Concerns/Sortable.php
+++ src/Concerns/Sortable.php
@@ -2,14 +2,16 @@
 
 namespace Baro\PipelineQueryCollection\Concerns;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Pipeline\Pipeline;
 
 trait Sortable
 {
-    public function scopeSort($query)
+    public function scopeSort(Builder $query, array $criteria = null): Builder
     {
+        $criteria = is_null($criteria) ? $this->getSorts() : $criteria;
         return app(Pipeline::class)
             ->send($query)
-            ->through($this->getSorts());
+            ->through($criteria);
     }
 }
